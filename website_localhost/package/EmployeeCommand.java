6f95484d6d42405302f13c7f5e04496e12db4968
Manhtq <trinhquocmanh1990@gmail.com>
2022-04-14 09:45:59 +0700

    sp

---
+++ website_localhost/package/EmployeeCommand.java
@@ -0,0 +1,14 @@
+package com.example.architecture.hexagonal;
+public class EmployeeCommand{
+   public String entityClass;
+   public String getEntityClass() {
+      return entityClass;
+   }
+   public void setEntityClass(String entityClass) {
+      this.entityClass = entityClass;
+   }
+   @Override
+   public String toString() {
+      return "EmployeeCommand [entityClass=" + entityClass + "]";
+   }
+}
---
+++ website_localhost/package/EmployeeDomainDao.java
@@ -0,0 +1,17 @@
+package com.example.architecture.hexagonal;
+
+public class EmployeeDomainDao<T,TCommand>{ 
+   IPersists<T,TCommand> adapter;
+   public void save(T t,TCommand commandObject)   {
+      adapter.save(t, commandObject);
+   }
+   public void delete(T t,TCommand commandObject)   {
+      adapter.delete(t, commandObject);
+   }
+   public IPersists<T, TCommand> getAdapter() {
+      return adapter;
+   }
+   public void setAdapter(IPersists<T, TCommand> adapter) {
+      this.adapter = adapter;
+   }
+}
